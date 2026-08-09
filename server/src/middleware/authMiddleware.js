a233d51bf5bcc8fbed63baa4378a29ac27a62219
Aung Phyo Han <doubleaung5@gmail.com>
2026-08-09 19:18:19 -0300

    add role based auth

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -38,3 +38,22 @@ export const protect = async (req, res, next) => {
         });
     }
 }
+
+export const authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: "Not authenticated",
+            });
+        }
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: "You do not have permission to access this resource",
+            });
+        }
+
+        next();
+    };
+}
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,12 +5,32 @@ import {
     getMe,
 } from "../controllers/authController.js";
 
-import { protect } from "../middleware/authMiddleware.js";
+import {
+    protect,
+    authorize,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+//get routes
 router.get("/me", protect, getMe);
+router.get("/nurse-test", protect, authorize("nurse"), (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the nurse area",
+        user: req.user,
+    });
+});
+
+router.get("/admin-test", protect, authorize("admin"), (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the admin area",
+        user: req.user,
+    });
+});
 
 export default router;
